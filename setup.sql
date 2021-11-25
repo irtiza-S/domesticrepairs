c73ffaeb11f289053360f63799c18663b9bfdfcd
irtiza safdar <safdari@coventry.ac.uk>
2021-11-25 18:49:22 +0000

    geolocation data work in progress - trying to save data to the backend

--- setup.sql
+++ setup.sql
@@ -9,6 +9,8 @@ CREATE TABLE IF NOT EXISTS accounts (
   id MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   user VARCHAR(25) NOT NULL,
   pass VARCHAR(70) NOT NULL,
+  locationLongitude DECIMAL(11, 8),
+  locationLatitude DECIMAL(10, 8)
 );
 
 INSERT INTO accounts(user, pass)
