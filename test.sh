cc0deccf0d1356529810c9fe12e6d51d3bd70556
irtiza safdar <safdari@coventry.ac.uk>
2021-11-28 16:14:09 +0000

    test commit

--- test.sh
+++ test.sh
@@ -10,7 +10,21 @@ then
 	exit 1
 fi
 
-deno lint --unstable
+# CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+# echo "current branch: ${CURRENT_BRANCH}"
 
-echo $?
-echo "script running"
+# clear  #1. clear the terminal
+
+# deno run --allow-all --unstable index.js #run the web server and launch the website
+
+
+# Get the commit message from file, which just contains the message
+read -p 'enter your commit: ' commitMessage
+
+
+#check the length of the message - if it's less than 10, send a warning to rewrite the commit msg since it's not detailed enough
+
+
+
+
+# echo "script running"
